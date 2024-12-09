a692dd48cb065b2d4c1026a3430edcb3586ebcf9
Midhun Eldose <midhun.eldose@NEURA-1065.nad.neura-robotics.com>
2024-12-09 17:24:37 +0100

    quaternion.py

--- quaternion.py
+++ quaternion.py
@@ -259,6 +259,7 @@ if SOCKET_INTERFACE:
     from neurapy.socket_interface.robot import Robot
 else:
     from neurapy.robot import Robot
+    
 import pytest
 from neurapy.exceptions import IKNotFound
 
@@ -292,8 +293,7 @@ r = Robot()
 target_pose = r.ik_fk("fk", target_angle = [0.2,0.2,0.2,0.2,0.2,0.2])
 target_angle = r.ik_fk("ik", target_pose = [0.140448, -0.134195, 1.197456, 3.1396, -0.589, -1.025],current_joint = [-1.55, -0.69, 0.06, 1.67, -0.02, -1.57, 0.11])
 
-##########################################################################################################
-
+########### EXAMPLE FROM CHATGPT #############################################################
 from neurapy.robot import Robot
 
 r= Robot()
