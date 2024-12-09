880f19926bbb15b1bfb5dd82218fd34cacfd6f5b
Midhun Eldose <midhun.eldose@NEURA-1065.nad.neura-robotics.com>
2024-12-09 17:12:47 +0100

    quaternion.py

--- quaternion.py
+++ quaternion.py
@@ -1,32 +1,32 @@
 #  Quaternion to roll pitch yaw
 
-    from neurapy.robot import Robot
-    r = Robot()
-    rpy = r.quaternion_to_rpy(0.85,0,0.52,0)
+from neurapy.robot import Robot
+r = Robot()
+rpy = r.quaternion_to_rpy(0.85,0,0.52,0)
 
 #   Roll pitch yaw to quaternion
 
 
-    from neurapy.robot import Robot
-    r = Robot()
-    quaternion = r.rpy_to_quaternion(0,1.1,0)
+from neurapy.robot import Robot
+r = Robot()
+quaternion = r.rpy_to_quaternion(0,1.1,0)
     
     
 ### Quaternion to roll pitch yaw
 
 
 
-    from neurapy.robot import Robot
-    r = Robot()
-    rpy = r.quaternion_to_rpy(0.85,0,0.52,0)
+from neurapy.robot import Robot
+r = Robot()
+rpy = r.quaternion_to_rpy(0.85,0,0.52,0)
 
 ### Roll pitch yaw to quaternion
 
 
 
-    from neurapy.robot import Robot
-    r = Robot()
-    quaternion = r.rpy_to_quaternion(0,1.1,0)
+from neurapy.robot import Robot
+r = Robot()
+quaternion = r.rpy_to_quaternion(0,1.1,0)
 
  ################################################################################
 import neurapy.motion_planner as mp
