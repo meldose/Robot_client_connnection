b1cb45edcd1d328199ca52caa1b6d0367e1762dc
meldose <meldose@gmial.com>
2024-12-16 10:37:44 +0100

    cartesian_to_joint.py

--- cartesian_to_joint.py
+++ cartesian_to_joint.py
@@ -5,7 +5,7 @@ r = Robot()
 
 
 
-target_angle = r.ik_fk("ik", target_pose =[-0.524, -0.352, 0.208, -3.02,-0.06,1.41], # object pose values in radians
+target_angle = r.ik_fk("ik", target_pose =[-0.511, -0.367, 0.241, -3.02,-0.06,1.41], # object pose values in radians
 current_joint = [0.63,0.35,-1.34,3.01,1.15,-0.76]) # current joint angles in radians
 print(target_angle)
 
