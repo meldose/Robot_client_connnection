1d0cc44b01d6a4e0d3a6f5a2406961e8c16a49c8
meldose <meldose@gmail.com>
2024-12-17 16:02:39 +0100

    photoneo.py

--- photoneo.py
+++ photoneo.py
@@ -17,31 +17,6 @@ pose = pose_translation + pose_rotation
 
 #joint angles = r.ik_fk("ik", pose )
 #r.move_joint()
-
-
-############################### DUMMY CAMERA #################################################
-
-import os
-
-from neurapy.camera.camera import Camera
-
-class DummyCamera(Camera):
-    def __init__(self, file_path='~/pose.txt'):
-        self.file_path = os.path.expanduser(file_path)
-
-    def read_pose_from_file(self) -> list:
-        try:
-            with open(self.file_path, 'r') as file:
-                line = file.readline().strip()
-                pose_data = [float(value) for value in line.split(',')]
-                return pose_data
-        except Exception as e:
-            print(f"Failed to read pose from file: {e}")
-            raise
-
-    def get_pose(self) -> list:
-        return self.read_pose_from_file()
-    
     
 
 #################### PHOTONEO CAMERA ##########################################################
