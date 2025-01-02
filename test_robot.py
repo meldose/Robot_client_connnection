1ace054c919b06021f7f58af40132bb4f8abb6cd
meldose <meldose@gmial.com>
2025-01-02 18:06:58 +0100

    cartesian_to_joint.py

--- test_robot.py
+++ test_robot.py
@@ -5,8 +5,7 @@ from neurapy.robot import Robot
 import time
 
 r = Robot()
-#r.gripper("on")
-# time.sleep(0.2)
+
 r.set_mode("Automatic")
 r.set_override(0.7)
 target_pose=[0.68481949781846, -0.6321197774300908, -0.848276904538431, 3.0107798218702837, 1.6245106608034727, -0.7382670573384716]
@@ -14,4 +13,3 @@ linear_property = {
     "target_joint": target_pose,
 }
 r.move_joint(**linear_property)
-# r.gripper("off")
