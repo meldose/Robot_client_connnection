3bc4978e17859c4cae0c5d64be98102c35a078ca
meldose <meldose@gmial.com>
2025-01-03 14:17:40 +0100

    cartesian_to_joint.py

--- quaternion_to_euler.py
+++ quaternion_to_euler.py
@@ -3,7 +3,7 @@ from neurapy.robot import Robot
 def convert_quaternion_to_euler_pose():
 
     r = Robot()
-    quaternion_pose = [-0.512, -0.417, 0.390,0.329,-0.531,0.416,0.661] # [X, Y, Z, W, EX, EY, EZ]
+    quaternion_pose = [-0.508,-0.389,0.110,0.079,0.852,0.517,-0.019] # [X, Y, Z, W, EX, EY, EZ]
     euler_pose = r.convert_quaternion_to_euler_pose(quaternion_pose)
     print(euler_pose)  # Output: [X, Y, Z, R, P, Y] with Euler angle values.
 
