6d5e1bc9240bdf7d25b0a3240a7cfd65bd8c11c1
meldose <meldose@gmial.com>
2025-01-03 12:14:06 +0100

    cartesian_to_joint.py

--- quaternion_to_euler.py
+++ quaternion_to_euler.py
@@ -3,7 +3,7 @@ from neurapy.robot import Robot
 def convert_quaternion_to_euler_pose():
 
     r = Robot()
-    quaternion_pose = [-0.51,-0.398,0.240,-0.286,0.463,0.434,0.718] # [X, Y, Z, W, EX, EY, EZ]
+    quaternion_pose = [-0.512, -0.417, 0.390,0.329,-0.531,0.416,0.661] # [X, Y, Z, W, EX, EY, EZ]
     euler_pose = r.convert_quaternion_to_euler_pose(quaternion_pose)
     print(euler_pose)  # Output: [X, Y, Z, R, P, Y] with Euler angle values.
 
--- test_robot.py
+++ test_robot.py
@@ -8,8 +8,10 @@ r = Robot()
 
 r.set_mode("Automatic")
 r.set_override(0.7)
-target_pose=[0.68481949781846, -0.6321197774300908, -0.848276904538431, 3.0107798218702837, 1.6245106608034727, -0.7382670573384716]
+r.gripper("on")
+target_pose=[0.7192742243584301, -0.5876637224953866, -1.0873323798202241, 3.0085883961377955, 1.4361029088726027, -2.2493009438456437]
 linear_property = {
     "target_joint": target_pose,
 }
 r.move_joint(**linear_property)
+r.gripper("off")
