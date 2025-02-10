31408b5acfc59d0ed4466af444c7da2dcf5ce2e8
meldose <meldose@gmail.com>
2025-02-10 21:02:57 +0100

    CommunicationLibrary.py

--- CommunicationLibrary.py
+++ CommunicationLibrary.py
@@ -229,8 +229,7 @@ class ServoX:
 
         r.stop() # stopping the robot
         
-    
-ServoX(robot=r).servo_x()
+ServoX(robot=r).servo_x
         
 class ResponseHeader: # class used for storing data
     def __init__(self, request_id, sub_headers): # initializing the class
