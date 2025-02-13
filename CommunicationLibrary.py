16e2b9357daeffccfd6ece88db49c5e77990443e
meldose <meldose@gmial.com>
2025-02-13 08:51:30 +0100

    added new line for returning messages

--- CommunicationLibrary.py
+++ CommunicationLibrary.py
@@ -730,8 +730,7 @@ class RobotRequestResponseCommunication: # class used for storing data
             print('\033[94m' + "OBJECT: " + '\033[0m' + "[" + str(round(self.message[0], 3)) + "," + str(
                 round(self.message[1], 3)) + "," + str(round(self.message[2], 3)) + "," + str(
                 round(self.message[3], 3)) + "," + str(round(self.message[4], 3)) + "," + str(
-                round(self.message[5], 3)) + "," + str(round(self.message[6], 3)) + "]"
-
+                round(self.message[5], 3)) + "," + str(round(self.message[6], 3)) + "]")
         return self.message
 
 # -------------------------------------------------------------------
