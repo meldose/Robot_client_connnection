9ba8f5fa157031d989e060f1ee0c6748df8829ce
meldose <meldose@gmial.com>
2025-02-26 09:33:14 +0100

    CommunicationLibrary.py

--- CommunicationLibrary.py
+++ CommunicationLibrary.py
@@ -217,11 +217,11 @@ class ServoX: # defining servoX
         inp.target_velocity = [0.]*cart_pose_length # defning the target velocity
         inp.target_acceleration = [0.]*cart_pose_length # definng the target acceleration
 
-        inp.max_velocity = [80.0]*cart_pose_length # setting the maximum velocity with 0.5 times the cart pose lenght 
-        inp.max_acceleration = [70.0]*cart_pose_length #se tting the max acceleration with 3 times the cart pose length
-        inp.max_jerk = [20.0]*cart_pose_length # setting the jerk values
+        inp.max_velocity = [60.0]*cart_pose_length # setting the maximum velocity with 0.5 times the cart pose lenght 
+        inp.max_acceleration = [50.0]*cart_pose_length #se tting the max acceleration with 3 times the cart pose length
+        inp.max_jerk = [5.0]*cart_pose_length # setting the jerk values
 
-        servox_proportional_gain = 5 # setting the servox propotional gain as 25
+        servox_proportional_gain = 20 # setting the servox propotional gain as 25
 
         velocity = [0.] * 6 #Since ruckig does not provide rotational velocity if quaternion is input, we can send 0 rotational feedforward velocity
         acceleration = [0.] * 6 #Since ruckig does not provide rotational acceleration if quaternion is input, we can send 0 rotational feedforward acceleration
