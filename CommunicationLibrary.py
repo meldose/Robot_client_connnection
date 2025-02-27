dbe2815e8a7afe31d15719fab4eca48fd03e7389
meldose <meldose@gmial.com>
2025-02-27 13:15:34 +0100

    setting time sleep

--- CommunicationLibrary.py
+++ CommunicationLibrary.py
@@ -388,12 +388,12 @@ class ServoX: # defining servoX
         velocity = [0.3]*6 # setting the velocity 
         acceleration = [2.0]*6 # setting the acceleration
         target = copy.deepcopy(r.get_current_cartesian_pose()) # getting the current cartesian poses
-        time.sleep(1.0) # setting the time
+        time.sleep(1.5) # setting the time
 
         # target=new_message # setting the target position 
         target = [new_message[0], new_message[1], new_message[2], target[3], target[4], target[5], target[6]]
         error_code = r.movelinear_online(target, velocity, acceleration) # moving the robot
-        time.sleep(1.0) # setting the time
+        time.sleep(1.5) # setting the time
 
         r.deactivate_servo_interface() # deactivating the servo interface
         r.gripper("off") # setting gripper close position
