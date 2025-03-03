157e4bc3115ff355e4a8c2d1196cc2339fa1f74f
meldose <meldose@gmial.com>
2025-03-03 17:02:26 +0100

    added new quaternion values

--- cartesian_to_joint.py
+++ cartesian_to_joint.py
@@ -1,7 +1,7 @@
 
 from neurapy.robot import Robot # import the robot class
 r = Robot() # create an instance of the robot class
-target_angle = r.ik_fk("ik", target_pose =[-0.564, -0.383, 0.055, 0.08314504405453564, -3.1383167575436444, 1.0277256508563481],
+target_angle = r.ik_fk("ik", target_pose =[-0.455, -0.447, 0.065, 0.07770435578717998, -3.1345624628521964, 0.9698778755924939],
 current_joint=[0.4129863573167266, -0.040367615708926226, -1.603394842405772,-0.07106004628138933, -1.5406368975914917, -2.2229109243340384])
 
 print(target_angle) # print the target joint angles
--- quaternion_to_euler.py
+++ quaternion_to_euler.py
@@ -5,7 +5,7 @@ def convert_quaternion_to_euler_pose(): # defining the function for convertin th
 
     r = Robot() # setting the RObot class
     # quaternion_pose = new_message # [X, Y, Z, W, EX, EY, EZ]
-    quaternion_pose = [-0.564,-0.383,0.055,0.019,-0.491,0.87,-0.037]
+    quaternion_pose = [-0.455,-0.447,0.065,0.015,-0.466,0.884,-0.036]
     euler_pose = r.convert_quaternion_to_euler_pose(quaternion_pose) # getting te euler pose
     print(euler_pose)  # Output: [X, Y, Z, R, P, Y] with Euler angle values.
 
