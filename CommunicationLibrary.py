f439096e5bb4a93da4237a5544b6cf169dfea86a
meldose <meldose@gmail.com>
2025-03-04 18:48:24 +0100

    CommunicationLibrary.py

--- CommunicationLibrary.py
+++ CommunicationLibrary.py
@@ -132,14 +132,14 @@ class ServoJ:  # defining servoJ
 
         target_end_effector_pose = euler_pose # setting the target end effector pose
         reference_joint_angles = r.get_current_joint_angles() # getting the current joint angles
-        joint_angle_solution = r.compute_inverse_kinematics(target_end_effector_pose, reference_joint_angles)
+        joint_angle_solution = r.compute_inverse_kinematics(target_end_effector_pose, reference_joint_angles) # computing the inverse kinematics
 
         print("Target Joint Angles:", joint_angle_solution) # print the target joint angles
-
-        inp.target_position = joint_angle_solution # setting the target position
+        
+        inp.target_position = joint_angle_solution # setting the target position 
         target = copy.deepcopy(inp.current_position) # copying the current position of the robot 
         inp.target_position = new_message
-        # inp.target_position = [new_message[0], new_message[1], new_message[2], target[3], target[4], target[5], target[6]] # passing the values by fixing the [X,Y,z and fixign the d,a,b,c]
+        # inp.target_position = [new_message[0], new_message[1], new_message[2], target[3], target[4], target[5], target[6]] # passing the values by fixing the [X,Y,z and fixing the d,a,b,c]
         inp.target_acceleration = [0.0] * dof # setting the target acceleration as zero
         inp.max_velocity = [0.8] * dof #    defining the maximum velocity
         inp.max_acceleration = [7.0] * dof # defining the maximum acceleration
