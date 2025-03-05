68ea3a125f6b57ad117f3dfd4dd8aa22f8dcac20
meldose <meldose@gmial.com>
2025-03-05 13:38:57 +0100

    improved speed and acceleration

--- CommunicationLibrary.py
+++ CommunicationLibrary.py
@@ -218,8 +218,8 @@ class ServoX: # defining servoX
         inp.target_velocity = [0.]*cart_pose_length # defning the target velocity
         inp.target_acceleration = [0.]*cart_pose_length # definng the target acceleration
 
-        inp.max_velocity = [60.0]*cart_pose_length # setting the maximum velocity with 0.5 times the cart pose lenght 
-        inp.max_acceleration = [50.0]*cart_pose_length #se tting the max acceleration with 3 times the cart pose length
+        inp.max_velocity = [80.0]*cart_pose_length # setting the maximum velocity with 0.5 times the cart pose lenght 
+        inp.max_acceleration = [70.0]*cart_pose_length #se tting the max acceleration with 3 times the cart pose length
         inp.max_jerk = [5.0]*cart_pose_length # setting the jerk values
 
         servox_proportional_gain = 30 # setting the servox propotional gain as 25
