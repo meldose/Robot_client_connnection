08bca671e96949a661f8c5e5eafeece959e3fb2c
meldose <meldose@gmial.com>
2025-03-17 14:51:16 +0100

    CommunicationLibrary.py

--- CommunicationLibrary.py
+++ CommunicationLibrary.py
@@ -462,8 +462,8 @@ class RobotRequestResponseCommunication: # class used for storing data
         # Assuming response contains object data in dictionary format
         if response is not None:
             response["timestamp"] = timestamp  # Add timestamp to response
-            # return response
             print(f"The timestamp is :",response)
+            return response
         else:
             return None
 
